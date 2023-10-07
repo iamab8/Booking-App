d98b7594476a7b92bfec8d12e8d085b1acee5c44
iamab8 <anuragbhardwaj3108@gmail.com>
2023-10-07 20:54:51 +0530

    till 50min

--- api/index.js
+++ api/index.js
@@ -5,26 +5,28 @@ import authRoute from "./routes/auth.js";
 import usersRoute from "./routes/users.js";
 import hotelsRoute from "./routes/hotels.js";
 import roomsRoute from "./routes/rooms.js";
+import cookieParser from "cookie-parser";
+import cors from "cors";
 
 const app = express();
-
 dotenv.config();
 
 const connect = async () => {
   try {
-    await mongoose.connect(process.env.MONGODB);
-    console.log("Connected to MongoDB");
-  } catch (err) {
-    throw err;
+    await mongoose.connect(process.env.MONGO);
+    console.log("Connected to mongoDB.");
+  } catch (error) {
+    throw error;
   }
 };
 
 mongoose.connection.on("disconnected", () => {
-  console.log("mongoodb disconnected");
+  console.log("mongoDB disconnected!");
 });
 
-// middleware
-
+//middlewares
+app.use(cors())
+app.use(cookieParser())
 app.use(express.json());
 
 app.use("/api/auth", authRoute);
@@ -34,7 +36,7 @@ app.use("/api/rooms", roomsRoute);
 
 app.use((err, req, res, next) => {
   const errorStatus = err.status || 500;
-  const errorMessage = err.message || "Something went wrong";
+  const errorMessage = err.message || "Something went wrong!";
   return res.status(errorStatus).json({
     success: false,
     status: errorStatus,
@@ -45,5 +47,5 @@ app.use((err, req, res, next) => {
 
 app.listen(8800, () => {
   connect();
-  console.log("Connecting to server... 8800");
+  console.log("Connected to backend.");
 });
